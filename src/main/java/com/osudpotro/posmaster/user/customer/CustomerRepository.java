b9412e84d6689be441045845fb0085ceb8d2926c
duruliu72 <duruliu72@gmail.com>
2026-05-06 12:06:24 +0600

    fixed conflict

--- src/main/java/com/osudpotro/posmaster/user/customer/CustomerRepository.java
+++ src/main/java/com/osudpotro/posmaster/user/customer/CustomerRepository.java
@@ -1,6 +1,5 @@
 package com.osudpotro.posmaster.user.customer;
 
-import com.osudpotro.posmaster.user.Employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
