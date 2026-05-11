3da5a559c0ad7974b121deb1af46ee9eec5e1249
duruliu72 <duruliu72@gmail.com>
2026-05-11 12:15:37 +0600

    slaeStatus entity created

--- src/main/java/com/osudpotro/posmaster/sale/SaleDto.java
+++ src/main/java/com/osudpotro/posmaster/sale/SaleDto.java
@@ -22,7 +22,8 @@ public class SaleDto {
     private BigDecimal deliveryFee;
     private String prescriptionDocs;
     private Integer saleChannel;
-    private Integer saleStatus;
+    //private Integer saleStatus;
+    private SaleStatus saleStatus;
     private Integer paymentStatus;
     private Integer saleType;
     private UserPlainDto customer;
